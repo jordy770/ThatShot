5410eca18580944de9511dbb7c508ae0b53df0a1
jordy770 <32267019+jordy770@users.noreply.github.com>
2018-10-08 13:05:11 +0200

    update photo's

---
+++ ThatShot/ThatShot/Models/Photos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThatShot.Models
+{
+    public class Photos
+    {
+
+        public int Id{ get; set; }
+
+        public string Photo{ get; set; }
+    }
+}
