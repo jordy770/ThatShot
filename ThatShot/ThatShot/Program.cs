0ec59f8d94bd5702b8de549fa8b159546a50e327
jordy770 <32267019+jordy770@users.noreply.github.com>
2018-10-10 14:40:02 +0200

    Update Roles
    Working in progress

--- ThatShot/ThatShot/Program.cs
+++ ThatShot/ThatShot/Program.cs
@@ -22,3 +22,35 @@ namespace ThatShot
                 .UseStartup<Startup>();
     }
 }
+
+
+/*public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        var host = BuildWebHost(args);
+        using (var scope = host.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            try
+            {
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                DbInitializer.InitializerAsync(context, services).Wait();
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred while seeding the database.");
+            }
+            host.Run(); ;
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .Build();
+        }
+    }
+}*/
